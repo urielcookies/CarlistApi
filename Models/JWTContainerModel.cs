96ca339783fc58e6a430974b5ee7a58a4bbf7d6c
Ever Uriel Garcia <uriel621@live.com>
2019-12-27 20:23:44 -0700

    Set up JWT

---
+++ Models/JWTContainerModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationService.Models
+{
+    public class JWTContainerModel : IAuthContainerModel
+    {
+        #region Public Methods
+        public int ExpireMinutes { get; set; } = 525600; // 1 Year.
+        public string SecretKey { get; set; } = "EVERf3JzWbTSP8GEXMvrzpTyURIELRMwRyYwSVkfHRzNDsgycAjwHqchyJGARCIA"; // This secret key should be moved to some configurations outter server.
+        public string SecurityAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+
+        public Claim[] Claims { get; set; }
+        #endregion
+    }
+}
